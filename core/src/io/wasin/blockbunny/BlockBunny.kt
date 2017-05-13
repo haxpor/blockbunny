6b11d8efec98a078d86883fcf71f2cdb63be0493
Wasin Thonkaew <haxpor@gmail.com>
2017-05-13 17:27:45 +0800

    Set up kotlin in build process. Converted a single file to Kotlin as test-drive.
    Notable is that
    - need to set gradle version in gradle-wrapper.properties to
      https\://services.gradle.org/distributions/gradle-2.10-all.zip
    - in parent build.gradle uses classpath
      'com.android.tools.build:gradle:1.5.0'

---
+++ core/src/io/wasin/blockbunny/BlockBunny.kt
@@ -0,0 +1,30 @@
+package io.wasin.blockbunny
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class BlockBunny : ApplicationAdapter() {
+    lateinit internal var batch: SpriteBatch
+    lateinit internal var img: Texture
+
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render() {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
