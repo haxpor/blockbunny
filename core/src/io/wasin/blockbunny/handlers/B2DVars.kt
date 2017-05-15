128a36a5969e701960f8fff300dda17089f9cbed
Wasin Thonkaew <haxpor@gmail.com>
2017-05-16 01:26:04 +0800

    Testing code with box2d object falling to platform
    It involves static, and dynamic object with the demonstration code to
    initialize world, body, shape, and fixture for both a falling object,
    and platform.
    
    This takes into account of PPM or pixel per meters that needs to be used
    for convertion when dealing with units.

---
+++ core/src/io/wasin/blockbunny/handlers/B2DVars.kt
@@ -0,0 +1,10 @@
+package io.wasin.blockbunny.handlers
+
+/**
+ * Created by haxpor on 5/16/17.
+ */
+class B2DVars {
+    companion object {
+        const val PPM: Float = 100f
+    }
+}
--- core/src/io/wasin/blockbunny/states/Play.kt
+++ core/src/io/wasin/blockbunny/states/Play.kt
@@ -1,6 +1,12 @@
 package io.wasin.blockbunny.states
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont
+import io.wasin.blockbunny.handlers.B2DVars
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.*
+import io.wasin.blockbunny.Game
 import io.wasin.blockbunny.handlers.GameStateManager
 
 /**
@@ -8,23 +14,54 @@ import io.wasin.blockbunny.handlers.GameStateManager
  */
 class Play(gsm: GameStateManager) : GameState(gsm) {
 
-    private var font: BitmapFont
+    private var world: World
+    private var b2dr: Box2DDebugRenderer
+    private var b2dCam: OrthographicCamera
 
     init {
-        this.font = BitmapFont()
+        world = World(Vector2(0f, -9.81f), true)
+        b2dr = Box2DDebugRenderer()
+
+        // create platform
+        val bdef = BodyDef()
+        bdef.position.set(160.0f / B2DVars.PPM, 120.0f / B2DVars.PPM)
+        bdef.type = BodyDef.BodyType.StaticBody
+
+        var body = world.createBody(bdef)
+
+        val shape = PolygonShape()
+        shape.setAsBox(50f / B2DVars.PPM, 5f / B2DVars.PPM)
+
+        val fdef = FixtureDef()
+        fdef.shape = shape
+        body.createFixture(fdef)
+
+        // create falling box
+        bdef.position.set(160f / B2DVars.PPM, 200f / B2DVars.PPM)
+        bdef.type = BodyDef.BodyType.DynamicBody
+        body = world.createBody(bdef)
+        shape.setAsBox(5f / B2DVars.PPM,5f / B2DVars.PPM)
+        fdef.shape = shape
+        body.createFixture(fdef)
+
+        // set box2d cam
+        b2dCam = OrthographicCamera()
+        b2dCam.setToOrtho(false, Game.V_WIDTH / B2DVars.PPM, Game.V_HEIGHT / B2DVars.PPM)
     }
 
     override fun handleInput() {
     }
 
     override fun update(dt: Float) {
+        world.step(dt, 6, 2)
     }
 
     override fun render() {
-        sb.projectionMatrix = cam.combined
-        sb.begin()
-        font.draw(sb, "Play state", 100.0f, 100.0f)
-        sb.end()
+        // clear screen
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        // draw box2d world
+        b2dr.render(world, b2dCam.combined)
     }
 
     override fun dispose() {
