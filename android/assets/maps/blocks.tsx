97b5c001de117d400427fa7c9afc405d52dd4784
Wasin Thonkaew <haxpor@gmail.com>
2017-05-29 22:48:08 +0800

    Fix affected box2d debug drawing. Support 2 levels with crystals.
    This changes enabled box2d debug draw now.
    It fixed the code section that draws box2d debug drawing on top of
    normal content as it doesn't need SpriteBatch's begin()/end() block to
    wrap around.

---
+++ android/assets/maps/blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="blocks" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="blocks.png" width="96" height="32"/>
+</tileset>
